39c3f1ec193e6c920731bfa02e1c90f2a96bb5d2
nihal369 <nihalismailk@gmail.com>
2018-01-14 12:55:22 +0530

    Detects user's current position

--- app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/MainActivity.java
+++ app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/MainActivity.java
@@ -1,28 +1,35 @@
 package com.disastermanagment_vjc.www.disastermanagmentorganization;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private int MY_PERMISSIONS_REQUEST_FINE_LOCATION;
+    private double lattitude,longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Log.i("NIHAL",LocalDB.getEmailAddress());
-        Log.i("NIHAL",LocalDB.getFullName());
-        //Log.i("NIHAL",LocalDB.getPhoneNumber());
-        //Log.i("NIHAL",LocalDB.getProfilePicUri().toString());
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -39,13 +46,39 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+
+                    return;
+            }
+
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                lattitude=location.getLatitude();
+                                longitude=location.getLongitude();
+                                LatLng userLatLng=new LatLng(lattitude,longitude);
+                                mMap.addMarker(new MarkerOptions().position(userLatLng).title(LocalDB.getFullName()));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng));
+                            }
+                        }
+                    });
+        }
 }
