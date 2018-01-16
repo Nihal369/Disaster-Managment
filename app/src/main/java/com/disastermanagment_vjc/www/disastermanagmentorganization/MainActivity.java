e2560d512860d32b92d51fbb7354f823da332678
nihal369 <nihalismailk@gmail.com>
2018-01-16 18:41:39 +0530

    Going back to fix a bug

--- app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/MainActivity.java
+++ app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/MainActivity.java
@@ -15,7 +15,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -47,206 +46,192 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     Map<String,Marker>usersMarkersMap;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            //Function Objective:Load the layout and set initial things
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Function Objective:Load the layout and set initial things
+        requestPermissionFromUser();
+        usersMarkersMap=new HashMap<>();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //Function Objective:Display the Map with the app features
+        //Assign the Google Map to an Object so that we can use it in program
+        mMap = googleMap;
+
+        //Request the permission to access location from the user
+        requestPermissionFromUser();
+
+        try {
+            //Get the last known location in the user's device
+            getInitialLocationOfUser();
+
+            //Update the user position periodically both on map and on firebase
+            updateUserLocation();
+
+            //Read the database to get information about rest of the units
+            readDataFromFirebase();
+        }
+        catch (Exception e)
+        {
             requestPermissionFromUser();
-            usersMarkersMap=new HashMap<>();
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
         }
+    }
 
-        @SuppressLint("MissingPermission")
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-                //Function Objective:Display the Map with the app features
-                //Assign the Google Map to an Object so that we can use it in program
-                mMap = googleMap;
-
-                //Request the permission to access location from the user
-                requestPermissionFromUser();
-
-                try {
-                    //Get the last known location in the user's device
-                    getInitialLocationOfUser();
-
-                    //Update the user position periodically both on map and on firebase
-                    updateUserLocation();
-
-                    //Read the database to get information about rest of the units
-                    readDataFromFirebase();
-                }
-                catch (Exception e)
-                {
-                    requestPermissionFromUser();
-                }
-            }
-
-        private void requestPermissionFromUser(){
-                //Function Objective:Request location permission from the user
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                            MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-                }
-            }
+    private void requestPermissionFromUser(){
+        //Function Objective:Request location permission from the user
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
 
 
-        @SuppressLint("MissingPermission")
-        private void getInitialLocationOfUser() {
-            //Function Objective:Get initial position of user
-            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-            requestPermissionFromUser();
-            mFusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                // Logic to handle location object
-                                initalLat=location.getLatitude();
-                                initalLng=location.getLongitude();
-                            }
-
+    @SuppressLint("MissingPermission")
+    private void getInitialLocationOfUser() {
+        //Function Objective:Get initial position of user
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        requestPermissionFromUser();
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            initalLat=location.getLatitude();
+                            initalLng=location.getLongitude();
                         }
-                    });
-        }
 
-        private void updateUserLocation()
+                    }
+                });
+    }
+
+    private void updateUserLocation()
+    {
+        //Function Objective:Update user's location periodically
+
+        long mLocTrackingInterval = 1000 * 3; // Tracking interval set at 3 seconds = 3000 milli seconds
+        float trackingDistance = 0;//Minimum Tracking distance
+        LocationAccuracy trackingAccuracy = LocationAccuracy.HIGH;//Location accuracy set to high,Higher the accuracy higher will be the battery drain
+
+        //Location parameters
+        LocationParams.Builder builder = new LocationParams.Builder()
+                .setAccuracy(trackingAccuracy)
+                .setDistance(trackingDistance)
+                .setInterval(mLocTrackingInterval);
+
+
+
+        //Customising the marker
+        MarkerOptions markerOptions=new MarkerOptions().position(new LatLng(initalLat,initalLng)).title(LocalDB.getFullName());
+        final Marker marker=mMap.addMarker(markerOptions);
+
+        //SmartLocation Object tracks the position of the user accurately
+        SmartLocation.with(this)
+                .location()
+                .continuous()
+                .config(builder.build())
+                .start(new OnLocationUpdatedListener() {
+                    @Override
+                    public void onLocationUpdated(Location location) {
+                        latitude=location.getLatitude();
+                        longitude=location.getLongitude();
+                        updateFirebaseData(latitude,longitude);
+                        //Create a new user coordinate object
+                        LatLng userCoordinates=new LatLng(latitude,longitude);
+                        marker.setPosition(userCoordinates);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(userCoordinates));
+                    }
+                });
+    }
+
+    private void updateFirebaseData(double lat,double lng)
+    {
+        //Function Objective:Update the coordinates on the firebase real-time database
+        try
         {
-            //Function Objective:Update user's location periodically
-
-            long mLocTrackingInterval = 1000 * 3; // Tracking interval set at 3 seconds = 3000 milli seconds
-            float trackingDistance = 0;//Minimum Tracking distance
-            LocationAccuracy trackingAccuracy = LocationAccuracy.HIGH;//Location accuracy set to high,Higher the accuracy higher will be the battery drain
-
-            //Location parameters
-            LocationParams.Builder builder = new LocationParams.Builder()
-                    .setAccuracy(trackingAccuracy)
-                    .setDistance(trackingDistance)
-                    .setInterval(mLocTrackingInterval);
-
-
-
-            //Customising the marker
-            BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(getDrawablePathIcon(LocalDB.getUnitType()));
-            MarkerOptions markerOptions=new MarkerOptions().position(new LatLng(initalLat,initalLng)).title(LocalDB.getFullName()).icon(icon);
-            final Marker marker=mMap.addMarker(markerOptions);
-
-            //SmartLocation Object tracks the position of the user accurately
-            SmartLocation.with(this)
-                    .location()
-                    .continuous()
-                    .config(builder.build())
-                    .start(new OnLocationUpdatedListener() {
-                        @Override
-                        public void onLocationUpdated(Location location) {
-                            latitude=location.getLatitude();
-                            longitude=location.getLongitude();
-                            updateFirebaseData(latitude,longitude);
-                            //Create a new user coordinate object
-                            LatLng userCoordinates=new LatLng(latitude,longitude);
-                            marker.setPosition(userCoordinates);
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(userCoordinates));
-                        }
-                    });
-        }
-
-        private void updateFirebaseData(double lat,double lng)
-        {
-            //Function Objective:Update the coordinates on the firebase real-time database
-            try
-            {
-                getFirebaseReference();
-                userRef.child("lat").setValue(lat);
-                userRef.child("ln").setValue(lng);
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        private void getFirebaseReference()
-        {
-            //Function Objective:Create the firebase reference
-            mRootRef = FirebaseDatabase.getInstance().getReference();
-            unitRef = mRootRef.child("Units");
-            userRef=unitRef.child(LocalDB.getFullName());
-        }
-
-        private void readDataFromFirebase()
-        {
-            //Function Objective:Reads the data like unitType from firebase
             getFirebaseReference();
+            userRef.child("lat").setValue(lat);
+            userRef.child("ln").setValue(lng);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-            unitRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
+    private void getFirebaseReference()
+    {
+        //Function Objective:Create the firebase reference
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+        unitRef = mRootRef.child("Units");
+        userRef=unitRef.child(LocalDB.getFullName());
+    }
 
-                    //Map that stores retrived data from the DataSnapshot
-                    if(dataSnapshot!=null) {
-                        fireBaseMap = (HashMap<String, String>) dataSnapshot.getValue();
-                    }
+    private void readDataFromFirebase()
+    {
+        //Function Objective:Reads the data like unitType from firebase
+        getFirebaseReference();
 
-                    Log.i("NIHAL",fireBaseMap.toString());
-                    if(fireBaseMap!=null) {
+        unitRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                        //Retrieve data from the snapshot map
-                        for (String key : fireBaseMap.keySet()) {
+                //Map that stores retrived data from the DataSnapshot
+                if(dataSnapshot!=null) {
+                    fireBaseMap = (HashMap<String, String>) dataSnapshot.getValue();
+                }
 
-                            String value = String.valueOf(fireBaseMap.get(key));
-                            value = value.substring(1, value.length()-1);           //remove curly brackets
-                            String[] keyValuePairs = value.split(",");              //split the string to create key-value pairs
-                            Map<String,String> subMap = new HashMap<>();
+                Log.i("NIHAL",fireBaseMap.toString());
+                if(fireBaseMap!=null) {
 
-                            for(String pair : keyValuePairs)                        //iterate over the pairs
-                            {
-                                String[] entry = pair.split("=");                   //split the pairs to get key and value
-                                subMap.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
-                            }
+                    //Retrieve data from the snapshot map
+                    for (String key : fireBaseMap.keySet()) {
 
-                            //UPDATE MARKERS ON MAP
-                            if(usersMarkersMap.containsKey(key))
-                            {
-                                Marker marker = usersMarkersMap.get(key);
-                                marker.setPosition(new LatLng(Double.parseDouble(subMap.get("lat")), Double.parseDouble(subMap.get("ln")))); // Update your marker
-                            }
-                            else if(!key.equals(LocalDB.getFullName()))
-                            {
-                                BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(getDrawablePathIcon(subMap.get("unitType")));
-                                Marker usersMarker = mMap.addMarker(new MarkerOptions()
-                                        .position(new LatLng(Double.parseDouble(subMap.get("lat")), Double.parseDouble(subMap.get("ln"))))
-                                        .title(key)
-                                        .icon(icon));
+                        String value = String.valueOf(fireBaseMap.get(key));
+                        value = value.substring(1, value.length()-1);           //remove curly brackets
+                        String[] keyValuePairs = value.split(",");              //split the string to create key-value pairs
+                        Map<String,String> subMap = new HashMap<>();
 
-                                usersMarkersMap.put(key, usersMarker);
-                            }
+                        for(String pair : keyValuePairs)                        //iterate over the pairs
+                        {
+                            String[] entry = pair.split("=");                   //split the pairs to get key and value
+                            subMap.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
+                        }
+
+                        //UPDATE MARKERS ON MAP
+                        if(usersMarkersMap.containsKey(key))
+                        {
+                            Marker marker = usersMarkersMap.get(key);
+                            marker.setPosition(new LatLng(Double.parseDouble(subMap.get("lat")), Double.parseDouble(subMap.get("ln")))); // Update your marker
+                        }
+                        else if(!key.equals(LocalDB.getFullName()))
+                        {
+                            Marker usersMarker = mMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(Double.parseDouble(subMap.get("lat")), Double.parseDouble(subMap.get("ln"))))
+                                    .title(key)
+                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+
+                            usersMarkersMap.put(key, usersMarker);
                         }
                     }
                 }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-        }
-
-        private int getDrawablePathIcon(String unitTypeValue)
-        {
-            switch (unitTypeValue)
-            {
-                case "firefighter":return R.drawable.firetruck;
-                case "ambulance":return  R.drawable.ambulance;
-                case "rescuer":return R.drawable.rescuer;
-                case "victim": return R.drawable.victim;
             }
-            return R.drawable.logo;
-        }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 }
