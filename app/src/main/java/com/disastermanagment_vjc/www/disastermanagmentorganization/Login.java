70c6112400814373a983079ffec0aa096b0c5eba
nihal369 <nihalismailk@gmail.com>
2018-01-16 15:01:02 +0530

    Code refactoring

--- app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/Login.java
+++ app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/Login.java
@@ -2,21 +2,17 @@ package com.disastermanagment_vjc.www.disastermanagmentorganization;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
@@ -47,16 +43,23 @@ public class Login extends AppCompatActivity {
     //Object declarations
     SignInButton googleSignInButton;
     FirebaseAuth firebaseAuth;
+
     //Sign In Flag Code
     private static final int RC_SIGN_IN=2;
+
     GoogleApiClient mGoogleApiClient;
     FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference mRootRef,unitRef,userRef;
     Map<String, String> fireBaseMap;
 
+
+
+    //OnStart Function is created when the application is launched
     @Override
     protected void onStart() {
+        //Function Objective:Check whether the user has previously logged in during the launch of the application
         super.onStart();
+
         //Add a listener to check for a google account during app start
         if(isNetworkAvailable()) {
             firebaseAuth.addAuthStateListener(mAuthListener);
@@ -68,47 +71,23 @@ public class Login extends AppCompatActivity {
     }
 
 
-
+    //OnCreate is created when the activity is launched
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Function Objective: Configure the Google Sign In and Firebase options during the activity start
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         if (isNetworkAvailable()) {
-            //Decelerations
+            //Assign the buttons and text in the layout to an Object in the program
             googleSignInButton = findViewById(R.id.googleSignInButton);
 
-            TextView textView = (TextView) googleSignInButton.getChildAt(0);
-            textView.setTextColor(Color.BLACK);
-            textView.setText("Sign In With Google");
-
+            //Initialize firebase for this project
             FirebaseApp.initializeApp(this);
 
             //Check if a user has previously logged in using their google account
-            firebaseAuth = FirebaseAuth.getInstance();
-            mAuthListener = new FirebaseAuth.AuthStateListener() {
-                @Override
-                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                    //If there exists a signed in user
-                    if (firebaseAuth.getCurrentUser() != null) {
-                        //User has already logged in,Jump to Appointment activity
-                        FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                        //Assign user details to LocalDB
-
-                        LocalDB.setFullName(user.getDisplayName());
-                        LocalDB.setEmailAddress(user.getEmail());
-                        LocalDB.setPhoneNumber(user.getPhoneNumber());
-                        LocalDB.setProfilePicUri(user.getPhotoUrl());
-                        readDataFromFirebase();
-
-                        //Jump to appointment activity
-                        Intent intent = new Intent(Login.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
-            };
+            checkIfUserHasAlreadyLoggedIn();
 
 
             //OnClick listener of the google sign in button
@@ -120,21 +99,7 @@ public class Login extends AppCompatActivity {
             });
 
             // Configure Google Sign In
-            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.default_web_client_id))
-                    .requestEmail()
-                    .build();
-
-            // Build a GoogleApiClient with the options specified by gso.
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
-                        @Override
-                        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                            Toasty.error(Login.this, "Please Check Your Internet Connection and Try Again", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                    .build();
+           configureGoogleSignIn();
         }
         else
         {
@@ -142,88 +107,69 @@ public class Login extends AppCompatActivity {
         }
     }
 
+    private void checkIfUserHasAlreadyLoggedIn()
+    {
+        //Function Objective: Check if a user has logged in with their google account previously
+        //Get the firebase instance
+        firebaseAuth = FirebaseAuth.getInstance();
 
-    //SignIn Function
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        //Check the authentication in firebase
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //If there exists a signed in user
+                if (firebaseAuth.getCurrentUser() != null) {
+                    //User has already logged in,Jump to Main activity
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                    //Assign user details to LocalDB
+
+                    assignDetailsToLocalDB(user);
+
+                    //Get details from firebase like unitType which is not present in Google Account
+                    readDataFromFirebase();
+
+                    //Jump to Main activity
+                   jumpToMainActivity();
+                }
+            }
+        };
     }
 
-    //Result handler of the sign in function
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void configureGoogleSignIn()
+    {
+        //Function Objective:Configure the Google Sign In process
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if(result.isSuccess())
-            {
-                //Sign into google account
-                GoogleSignInAccount account=result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            }
-            else
-            {
-                Toasty.error(this, "Please Check Your Internet Connection and Try Again", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    //Link the google account to firebase
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        // Build a GoogleApiClient with the options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information;
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-
-
-                            Toasty.success(Login.this, "Success", Toast.LENGTH_SHORT).show();
-
-                            //Retrieve google account data and store it in LocalDB
-                            assert user != null;
-
-                            LocalDB.setFullName(user.getDisplayName());
-                            LocalDB.setEmailAddress(user.getEmail());
-                            LocalDB.setPhoneNumber(user.getPhoneNumber());
-                            LocalDB.setProfilePicUri(user.getPhotoUrl());
-                            readDataFromFirebase();
-
-
-                            //Continue user registration
-                            Intent menuIntent = new Intent(Login.this, MainActivity.class);
-                            startActivity(menuIntent);
-                            finish();
-                        }
-                        else {
-                            // If sign in fails, display a message to the user.
-                            Toasty.error(Login.this, "Please Check Your Internet Connection and Try Again",
-                                    Toast.LENGTH_SHORT).show();
-                        }
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toasty.error(Login.this, "Please Check Your Internet Connection and Try Again", Toast.LENGTH_SHORT).show();
                     }
-                });
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
-
-
-
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        assert connectivityManager != null;
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    private void assignDetailsToLocalDB(FirebaseUser user)
+    {
+        //Function Objective:Assign the Google Account details to the LocalDB class
+        LocalDB.setFullName(user.getDisplayName());
+        LocalDB.setEmailAddress(user.getEmail());
+        LocalDB.setPhoneNumber(user.getPhoneNumber());
+        LocalDB.setProfilePicUri(user.getPhotoUrl());
     }
 
     private void readDataFromFirebase()
     {
-        mRootRef = FirebaseDatabase.getInstance().getReference();
-        unitRef = mRootRef.child("Units");
-        userRef=unitRef.child(LocalDB.getFullName());
+        //Function Objective:Reads the data like unitType from firebase
+        getDataBaseReference();
+
         userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -257,6 +203,98 @@ public class Login extends AppCompatActivity {
 
     }
 
+    private void jumpToMainActivity()
+    {
+        //Function Objective:Move to Main Activity
+        //Intents are used to jump from one activity/application to another
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
+
+    private void signIn() {
+        //Function Objective:The Google Sign In UI is called by this function
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    //Result handler of the sign in function
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Function Objective: Result handler of the sign in function
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess())
+            {
+                //Sign into google account
+                GoogleSignInAccount account=result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+            else
+            {
+                Toasty.error(this, "Please Check Your Internet Connection and Try Again", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    //Link the google account to firebase
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        //Function Objective:Get the google account credentials like email and password to log in separately to Firebase
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+
+        //Sign into firebase with the recieved credentials
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information;
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+                            Toasty.success(Login.this, "Success", Toast.LENGTH_SHORT).show();
+
+                            //Retrieve google account data and store it in LocalDB
+                            assert user != null;
+
+                            assignDetailsToLocalDB(user);
+
+
+                            //Move to MainActivity
+                            jumpToMainActivity();
+                        }
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Toasty.error(Login.this, "Please Check Your Internet Connection and Try Again",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    private boolean isNetworkAvailable() {
+        //Function Objective: check whether Internet is available
+        //Initalize the Connectivity Manager Object
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connectivityManager != null;
+
+        //Connectivity Manager knows whether the phone is connected to Internet or not
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void getDataBaseReference()
+    {
+        //Function Objective:Create a reference to access the firebase database
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+        unitRef = mRootRef.child("Units");
+        userRef=unitRef.child(LocalDB.getFullName());
+    }
 
 }
--- app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/MainActivity.java
+++ app/src/main/java/com/disastermanagment_vjc/www/disastermanagmentorganization/MainActivity.java
@@ -1,14 +1,12 @@
 package com.disastermanagment_vjc.www.disastermanagmentorganization;
 
-import android.*;
 import android.Manifest;
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -30,14 +28,17 @@ import io.nlopez.smartlocation.location.config.LocationParams;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    //Object Decelerations
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
     private int MY_PERMISSIONS_REQUEST_FINE_LOCATION;
     private double initalLat,initalLng,latitude,longitude;
     private DatabaseReference mRootRef,unitRef,userRef;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Function Objective:Load the layout and set initial things
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -45,7 +46,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
 
     /**
@@ -58,70 +58,95 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-            mMap = googleMap;
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+                //Function Objective:Display the Map with the app features
+                //Assign the Google Map to an Object so that we can use it in program
+                mMap = googleMap;
 
-            // Add a marker in Sydney and move the camera
-            /*LatLng sydney = new LatLng(-34, 151);
-            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+                //Request the permission to access location from the user
+                requestPermissionFromUser();
 
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this,
-                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+                //Get the last known location in the user's device
+                getInitialLocationOfUser();
+
+                //Update the user position periodically both on map and on firebase
+                updateUserLocation();
+            }
+
+        private void requestPermissionFromUser(){
+                //Function Objective:Request location permission from the user
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_FINE_LOCATION);
                 }
+            }
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            initalLat=location.getLatitude();
-                            initalLng=location.getLongitude();
+
+        @SuppressLint("MissingPermission")
+        private void getInitialLocationOfUser() {
+            //Function Objective:Get initial position of user
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            requestPermissionFromUser();
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                initalLat=location.getLatitude();
+                                initalLng=location.getLongitude();
+                            }
+
                         }
+                    });
+        }
 
-                    }
-                });
+        private void updateUserLocation()
+        {
+            //Function Objective:Update user's location periodically
 
-        long mLocTrackingInterval = 1000 * 5; // 5 sec
-        float trackingDistance = 0;
-        LocationAccuracy trackingAccuracy = LocationAccuracy.HIGH;
+            long mLocTrackingInterval = 1000 * 3; // Tracking interval set at 3 seconds = 3000 milli seconds
+            float trackingDistance = 0;//Minimum Tracking distance
+            LocationAccuracy trackingAccuracy = LocationAccuracy.HIGH;//Location accuracy set to high,Higher the accuracy higher will be the battery drain
 
-        LocationParams.Builder builder = new LocationParams.Builder()
-                .setAccuracy(trackingAccuracy)
-                .setDistance(trackingDistance)
-                .setInterval(mLocTrackingInterval);
+            //Location parameters
+            LocationParams.Builder builder = new LocationParams.Builder()
+                    .setAccuracy(trackingAccuracy)
+                    .setDistance(trackingDistance)
+                    .setInterval(mLocTrackingInterval);
 
 
 
+            //Customising the marker
+            MarkerOptions markerOptions=new MarkerOptions().position(new LatLng(initalLat,initalLng)).title(LocalDB.getFullName());
+            final Marker marker=mMap.addMarker(markerOptions);
 
-        MarkerOptions markerOptions=new MarkerOptions().position(new LatLng(initalLat,initalLng)).title(LocalDB.getFullName());
-        final Marker marker=mMap.addMarker(markerOptions);
-
-        SmartLocation.with(this)
-                .location()
-                .continuous()
-                .config(builder.build())
-                .start(new OnLocationUpdatedListener() {
-                    @Override
-                    public void onLocationUpdated(Location location) {
-                        latitude=location.getLatitude();
-                        longitude=location.getLongitude();
-                        updateFirebaseData(latitude,longitude);
-                        LatLng user=new LatLng(latitude,longitude);
-                        marker.setPosition(user);
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(user));
-                    }
-                });
+            //SmartLocation Object tracks the position of the user accurately
+            SmartLocation.with(this)
+                    .location()
+                    .continuous()
+                    .config(builder.build())
+                    .start(new OnLocationUpdatedListener() {
+                        @Override
+                        public void onLocationUpdated(Location location) {
+                            latitude=location.getLatitude();
+                            longitude=location.getLongitude();
+                            updateFirebaseData(latitude,longitude);
+                            //Create a new user coordinate object
+                            LatLng userCoordinates=new LatLng(latitude,longitude);
+                            marker.setPosition(userCoordinates);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(userCoordinates));
+                        }
+                    });
         }
 
         private void updateFirebaseData(double lat,double lng)
         {
+            //Function Objective:Update the coordinates on the firebase real-time database
             try
             {
                 getFirebaseReference();
@@ -136,6 +161,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         private void getFirebaseReference()
         {
+            //Function Objective:Create the firebase reference
             mRootRef = FirebaseDatabase.getInstance().getReference();
             unitRef = mRootRef.child("Units");
             userRef=unitRef.child(LocalDB.getFullName());
